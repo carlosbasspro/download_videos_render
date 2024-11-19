dd6cd6ed5f405f2218e6adbc8abc5de703522ffa
carlosbasspro <117956780+carlosbasspro@users.noreply.github.com>
2024-11-19 20:44:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 
 @app.route('/download', methods=['POST'])
 def download():
+    '''
+    Essa função lida com requisições POST para baixar conteúdos do YouTube. 
+    O cliente deve enviar um JSON com o seguinte formato:
+
+    {
+        "url": "URL do vídeo do YouTube",
+        "format": "Formato desejado (ex: bestaudio/best ou bestvideo+bestaudio)",
+        "ext": "Extensão do arquivo de saída (ex: mp3 ou mp4)"
+    }
+
+    Respostas possíveis:
+    - Sucesso: Retorna o arquivo baixado como anexo.
+    - Erro: Retorna uma mensagem JSON com a descrição do erro.
+    '''
+
     data = request.json
     url = data.get('url')
     format = data.get('format')
@@ -14,47 +29,27 @@ def download():
     if not url or not format or not ext:
         return jsonify({"error": "URL, format, and extension are required!"}), 400
 
-    # Diretório atual
-    current_directory = os.getcwd()
-
-    # Criação do diretório download (se não existir)
-    download_dir = "download"
-    os.makedirs(download_dir, exist_ok=True)
 
     # Configurações do yt-dlp
     ydl_opts = {
         "format": f"{format}",
-        "outtmpl": f"{download_dir}/%(title)s.%(ext)s",
-        "cookiefile": "www.youtube.com_cookies.txt",
-        "sanitize_filename": True,
+        "outtmpl": "download/%(title)s.%(ext)s",
+       # "cookiefile": "www.youtube.com_cookies.txt",
     }
 
     try:
         with YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
-            video_title = info_dict.get("title", "video")
-            video_ext = info_dict.get("ext", ext)
-            video_path = os.path.join(download_dir, f"{video_title}.{video_ext}")
-            final_path = os.path.join(download_dir, f"{video_title}.{ext}")
-
-            # Verifique se o arquivo existe antes de renomear
-            if not os.path.exists(video_path):
-                return jsonify({
-                    "error": f"Arquivo não encontrado: {video_path}",
-                    "server_directory": current_directory
-                }), 500
-
-            # Renomear arquivo
+            video_title = info_dict.get("title", None)
+            video_ext = info_dict.get("ext", None)
+            video_path = f"./download/{video_title}.{video_ext}"
+            final_path = f"/storage/emulated/0/Download/BaixarTube Downloads/{video_title}.{ext}"
             os.rename(video_path, final_path)
 
             return send_file(final_path, as_attachment=True)
 
     except Exception as e:
-        # Retornar erro com diretório do servidor
-        return jsonify({
-            "error": str(e),
-            "server_directory": current_directory
-        }), 500
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
