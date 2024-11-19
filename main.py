a25bd78d93d509d7e73292190a2fa65568d872c3
carlosbasspro <117956780+carlosbasspro@users.noreply.github.com>
2024-11-19 20:57:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ def download():
         "outtmpl": "download/%(title)s.%(ext)s",
         "headers": {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+    },
+        "cookiesfrombrowser": "chrome"
        # "cookiefile": "www.youtube.com_cookies.txt",
     }
 
